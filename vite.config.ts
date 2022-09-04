c56d9258d090ec29ecca53a187619bafcedeb911
Carl Barrdahl <carlbarrdahl@gmail.com>
2022-09-04 14:45:44 +0200

    pwa updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,18 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "dreamachine",
+        short_name: "dreamachine",
+        description: "Dreamachine and Binaural beats",
+        theme_color: "#000000",
+        display_override: ["window-control-overlay", "minimal-ui"],
+        display: "standalone",
+      },
+    }),
+  ],
 });
